422eddaee02038c4f65f246a4ecf26b342b79b77
Andres G Molano <ingandresgamer14@gmail.com>
2023-11-11 11:38:46 -0500

    Se Ajustaron los archivos requirments.txt y  se ajusto el Notebook

---
+++ root.py
@@ -0,0 +1,5 @@
+import os
+DIR_ROOT=os.path.dirname(os.path.abspath(__file__))
+DIR_DATA="{0}{1}data{1}".format(DIR_ROOT, os.sep) 
+DIR_CHURN="{0}{1}churn{1}".format(DIR_ROOT, os.sep)
+DIR_NOTEBOOKS="{0}{1}notebooks{1}".format(DIR_ROOT, os.sep) 
